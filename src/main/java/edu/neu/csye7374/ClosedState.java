8b9eab747fc6d356ad8733502808e851a6b92739
neha-m98 <66993152+neha-m98@users.noreply.github.com>
2025-03-18 20:35:25 -0400

    updated closed state

--- src/main/java/edu/neu/csye7374/ClosedState.java
+++ src/main/java/edu/neu/csye7374/ClosedState.java
@@ -1,15 +1,26 @@
 package edu.neu.csye7374;
-
-public class ClosedState implements RestaurantState{
-
+ 
+import java.util.ArrayList;
+ 
+public class ClosedState implements RestaurantState {
+    private ArrayList<MenuItem> menuItems;
+ 
+    public ClosedState() {
+        this.menuItems = new ArrayList<>();
+    }
+ 
     @Override
     public void displayState() {
         System.out.println("Restaurant is currently Closed");
     }
-
+ 
     @Override
     public void displayMenu() {
         System.out.println("No Menu to display, Restaurant is Closed");
     }
-
+ 
+    @Override
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
 }

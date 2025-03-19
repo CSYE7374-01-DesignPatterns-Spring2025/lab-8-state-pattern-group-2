feee62d33f97acb248fbd72b4ea0fe9fa2cabaea
ZEEL PATEL <141486033+zeelapatel@users.noreply.github.com>
2025-03-18 20:33:00 -0400

    Update OpenState.java

--- src/main/java/edu/neu/csye7374/OpenState.java
+++ src/main/java/edu/neu/csye7374/OpenState.java
@@ -1,16 +1,40 @@
 package edu.neu.csye7374;
+ 
+import java.util.ArrayList;
+ 
+public class OpenState implements RestaurantState {
 
-public class OpenState implements RestaurantState{
+    private ArrayList<MenuItem> menuItems;
+ 
+    public OpenState() {
 
+        this.menuItems = new ArrayList<>();
 
-
+    }
+ 
     @Override
+
     public void displayState() {
+
         System.out.println("Restaurant is Open, Choose a menu option");
+
+    }
+ 
+    @Override
+
+    public void displayMenu() {
+
+        System.out.println("No Menu to display, Choose a menu option");
+
+    }
+ 
+    @Override
+
+    public ArrayList<MenuItem> getMenuItems() {
+
+        return menuItems;
+
     }
 
-    @Override
-    public void displayMenu() {
-        System.out.println("No Menu to display, Choose a menu option");
-    }
 }
+ 
